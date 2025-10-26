7c310259087a88e17ddc034fdba31b798a49c217
Silas <silas.progamacao@gmail.com>
2025-10-25 21:43:56 -0300

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,18 @@
 import { Elysia } from "elysia";
+import {z} from 'zod'
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+.get("/", () => "Hello Elysia")
+.get("/users/:id", ({params}) =>  {
+  const userId = params.id
+params: z.object({
+  id: z.string().uuid()
+})  
+
+}
+
+)
+.listen(3333);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
